cc91d85c8eb85d604e65781844162d7ad8a73ffb
S-nagar2002 <108257419+S-dhakad@users.noreply.github.com>
2025-11-05 00:24:21 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,91 +1,79 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  // images: {
-  //   remotePatterns: [
-  //     {
-  //       protocol: 'https',
-  //       hostname: 'example.com',
-  //       port: '',
-  //       pathname: '/**',
-  //     },
-  //     // Add other common image hosting domains that might be used
-  //     {
-  //       protocol: 'https',
-  //       hostname: '*.amazonaws.com',
-  //       port: '',
-  //       pathname: '/**',
-  //     },
-  //     {
-  //       protocol: 'https',
-  //       hostname: '*.cloudinary.com',
-  //       port: '',
-  //       pathname: '/**',
-  //     },
-  //     {
-  //       protocol: 'https',
-  //       hostname: '*.imgix.net',
-  //       port: '',
-  //       pathname: '/**',
-  //     },
-  //     {
-  //       protocol: 'https',
-  //       hostname: 'www.shutterstock.com',
-  //       port: '',
-  //       pathname: '/**',
-  //     },
-  //     {
-  //       protocol: 'https',
-  //       hostname: 'lh3.googleusercontent.com',
-  //     },
-  //     {
-  //       protocol: 'https',
-  //       hostname: 'img.youtube.com',
-  //       port: '',
-  //       pathname: '/**',
-  //     },
-  //     {
-  //       protocol: 'https',
-  //       hostname: 'i.ytimg.com',
-  //       port: '',
-  //       pathname: '/**',
-  //     },
-  //     {
-  //       protocol: 'https',
-  //       hostname: 'youtu.be',
-  //       port: '',
-  //       pathname: '/**',
-  //     },
-  //     {
-  //       protocol: 'https',
-  //       hostname: 'www.youtube.com',
-  //       port: '',
-  //       pathname: '/**',
-  //     },
-  //     {
-  //       protocol: 'https',
-  //       hostname: 'www.google.com',
-  //       pathname: '/**',
-  //     },
-  //     {
-  //       protocol: 'https',
-  //       hostname: 'www.facebook.com',
-  //       pathname: '/**',
-  //     },
-  //     {
-  //       protocol: 'https',
-  //       hostname: '**',
-  //     },
-  //     {
-  //       protocol: 'http',
-  //       hostname: 'localhost',
-  //     }
-  //   ],
-  // },
-
   images: {
-    unoptimized: false, // keep optimization
-  }
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'example.com',
+        port: '',
+        pathname: '/**',
+      },
+      // Add other common image hosting domains that might be used
+      {
+        protocol: 'https',
+        hostname: '*.amazonaws.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.cloudinary.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.imgix.net',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.shutterstock.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'img.youtube.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.ytimg.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'youtu.be',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.youtube.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.google.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.facebook.com',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
